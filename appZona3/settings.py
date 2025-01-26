4e52fe77455af60b1496c340e2e00fb53450a619
Jesus <jesusrincon0927@gmail.com>
2025-01-26 15:23:41 -0500

    gitignore database and file configuration

--- appZona3/settings.py
+++ appZona3/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = 'appZona3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'db_zona3x3', 
+        'USER': 'root',                 
+        'PASSWORD': '',          
+        'HOST': '127.0.0.1',                  
+        'PORT': '3306',                       
+
     }
 }
 
