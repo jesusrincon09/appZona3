a1f8dff85ba37740480784b4906a65acd53e2a6d
Jesus Rincon <jesusrincon0927@gmail.com>
2025-06-07 20:43:53 -0500

    implementation of the calendar for reservations

--- App/views/reservation.py
+++ App/views/reservation.py
@@ -1,16 +1,41 @@
 from django.urls import reverse_lazy
-from django.db.models import Prefetch
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import *
 from django.contrib import messages  
 from App.models import Reservation
 from App.forms import ReservationForm
+from django.http import JsonResponse
 
 class ListReservationView(ListView):
     model = Reservation
     template_name = "reservation/list.html"
     context_object_name = "reservations"
 
+class CalendarReservationView(ListView):
+    model = Reservation
+    template_name = "reservation/calendar.html"
+    context_object_name = "reservations"
+
+class ReservationEventsView(View):
+    def get(self, request, *args, **kwargs):
+        reservations = Reservation.objects.all()
+        events = []
+        for r in reservations:
+            events.append({
+                "id": r.id,
+                "title": r.client_name,
+                "start": r.reservation_start.isoformat(),
+                "end": r.reservation_end.isoformat(),
+                "extendedProps": {
+                    "client_name": r.client_name,
+                    "client_cell": r.client_cell,
+                    "client_email": r.client_email,
+                    "space": r.space.name if r.space else '',
+                }
+            })
+        return JsonResponse(events, safe=False)
+
+    
 class CreateReservationView(SuccessMessageMixin, CreateView):
     model = Reservation
     form_class = ReservationForm
